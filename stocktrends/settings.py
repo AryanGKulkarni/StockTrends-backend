abaad2adce8ad4b9e19838b8288e89b42b02e4fb
Aryan Kulkarni <aryangkulkarni@gmail.com>
2024-05-18 17:48:10 +0530

    allowed hosts

--- stocktrends/settings.py
+++ stocktrends/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*+@@wqu$rrjtl#@-!-s8i84r3+y0otj1%h8pt^bbje)%psh)e&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
